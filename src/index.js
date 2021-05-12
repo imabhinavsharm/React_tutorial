e8c9e2c0857ba8f68e4397c6dfda0f1633e7cdf2
imabhinavsharm <abhinavfeb16@gmail.com>
2021-05-12 14:17:12 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,49 @@
+// // ---------------------------------class - 1  11/05/2021-------------------------------------
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// // import './index.css';
+// // import App from './App';  
+// import reportWebVitals from './reportWebVitals';
+
+// // creating object to use css---------
+
+// // const heading_style = {
+// //   color:"teal",
+// //   fontSize:"80px"
+// // }
+
+// // define variable outside render always -------
+
+// // var day = "monday"
+// // if(day=="sunday"){
+// //   var message = "go to school"
+// // }
+// // else{
+// //   message = " there is holiday"
+// // }
+
+
+// ReactDOM.render(
+//   // use react fregments to wrap up all of your code inside <> --   </>  ------
+//   <>
+//     <h1 style ={heading_style}>Rishab</h1>
+
+//      {/* use {} for writing js inside jsx remember one thing you cant define variables in it  ------*/}
+//      {/* and you cant use if else there but we can use ternary operator ------- */}
+
+//     <h1>prathamesh {
+//     day=="sunday"? "there is a holiday" : "go to school"
+//       }</h1>
+
+//   </>
+//   ,
+//   document.getElementById('root')
+  
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+// reportWebVitals();
