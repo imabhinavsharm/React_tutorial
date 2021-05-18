0e485ec02cbb70c313386f6bd3934eeca3959845
imabhinavsharm <abhinavfeb16@gmail.com>
2021-05-18 14:42:17 +0530

    fifth

--- src/App.js
+++ src/App.js
@@ -3,7 +3,35 @@ import './App.css';
 //  creating and importing components
 import {Home} from './Home';
 // curly btraces for the components which are not exported deafault
-import Nav,{Name} from './Nav';
+import Nav from './Nav';
+import Contacts from './Contacts';
+import Service from './Service';
+import Card from './Card';
+// importing react router dom
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+// importing images
+import img1 from './img-1.png';
+import img2 from './img-2.jpg';
+import img3 from './img-3.png';
+
+const arr =[
+  {
+    imgsrc:img1,
+    title  : "Bilal"
+  },
+  {
+    imgsrc:img2,
+    title  : "Sourav"
+  },
+  {
+    imgsrc:img3,
+    title  : "rishab"
+  },
+]
 function App(){
   
   return (
@@ -26,9 +54,41 @@ function App(){
     //   </div>
     <>
     {/* using components */}
-    <Nav/>
-    <Home/>
-    <Name/>
+    // // ---------------------------------class - 4 15/05/2021-------------------------------------
+
+    <Router>
+      <Nav/>
+      <Switch>
+        <Route exact path = "/">
+         <Home 
+         heading = "hello"
+         />
+         <Home
+         heading = "world"
+         />
+         {
+          arr.map((cur,index)=>{
+            return (
+            <Card
+           imgsrc={cur.imgsrc}
+            titles={cur.title}
+            
+            />)
+          })
+
+         }
+         
+        
+        </Route>
+        <Route exact path = "/contact">
+        <Contacts/>
+        </Route>
+        <Route exact path = "/service">
+        <Service/>
+        </Route>
+      </Switch>
+
+    </Router>
     </>
   
     
